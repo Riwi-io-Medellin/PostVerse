73050d84cec1bc3e581d49a995747ba623d56c0c
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-11-12 08:09:59 -0500

    feat: Bug fixed in user routes

--- routes/api.php
+++ routes/api.php
@@ -9,13 +9,10 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::get('v1/users', [UserController::class,'index']);
-
-
-Route::group(function () {
+Route::prefix("v1")->middleware('guest:sanctum')->group(function () {
     Route::get('users', [UserController::class, 'index']);
     Route::post('users', [UserController::class, 'store']);
     Route::get('users/{id}', [UserController::class, 'show']);
     Route::put('users/{id}', [UserController::class, 'update']);
-    Route::delete('users/{id}', [UserController::class, 'destroy']); 
+    Route::delete('users/{id}', [UserController::class, 'destroy']);
 });
